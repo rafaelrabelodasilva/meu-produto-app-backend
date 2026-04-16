3ecf5e7756c80e9473ce66c45a42991f5c9a2167
Rafael Rabelo da Silva <rafaelrabelodasilva@outlook.com>
2026-04-16 17:30:35 -0300

    Corrigido CI

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 async function main() {
   const email = 'automacao_sistema@email.com';
