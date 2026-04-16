1c3cf432ca45f48f9116c650e8e1cf815cdae186
Rafael Rabelo da Silva <rafaelrabelodasilva@outlook.com>
2026-04-16 17:23:38 -0300

    Corrigido CI

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const email = 'automacao_sistema@email.com';
+  const password = 'Teste@1234';
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  console.log('Semeando banco de dados...');
+
+  const user = await prisma.user.upsert({
+    where: { email },
+    update: {},
+    create: {
+      email,
+      password: passwordHash,
+      firstName: 'Sistema',
+      lastName: 'Automacao',
+    },
+  });
+
+  console.log({ user });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
